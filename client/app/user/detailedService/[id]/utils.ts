b3db8abab2d3a3b5ae9e4f74e0eb04e7d835c5b7
nhieelam <lam.nhiee11@gmail.com>
2026-01-08 17:27:55 +0700

    up

--- client/app/user/detailedService/[id]/utils.ts
+++ client/app/user/detailedService/[id]/utils.ts
@@ -1,12 +1,17 @@
 import { Review } from "./types";
 import React from "react";
 
-export const renderStars = (rating: number): JSX.Element[] => {
-  return Array.from({ length: 5 }).map((_, i) => (
-    <span key={i} className={i < Math.floor(rating) ? "text-yellow-400 text-xl" : "text-gray-300 text-xl"}>
-      ⭐
-    </span>
-  ));
+export const renderStars = (rating: number): React.ReactElement[] => {
+  return Array.from({ length: 5 }).map((_, i) =>
+    React.createElement(
+      "span",
+      {
+        key: i,
+        className: i < Math.floor(rating) ? "text-yellow-400 text-xl" : "text-gray-300 text-xl",
+      },
+      "⭐"
+    )
+  );
 };
 
 export const calculateAverageRating = (reviews: Review[], defaultRating: number): number => {
