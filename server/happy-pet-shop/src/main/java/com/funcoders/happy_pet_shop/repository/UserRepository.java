dc6794fc45aa39788313f8b318de317a0b952961
Tran Dinh Khanh Du <trandinhkhanhdu2005@gmail.com>
2026-04-28 15:45:24 +0700

    update

--- server/happy-pet-shop/src/main/java/com/funcoders/happy_pet_shop/repository/UserRepository.java
+++ server/happy-pet-shop/src/main/java/com/funcoders/happy_pet_shop/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String name);
+    Optional<User> findByEmail(String email);
     boolean existsByUsername(String s);
 }
