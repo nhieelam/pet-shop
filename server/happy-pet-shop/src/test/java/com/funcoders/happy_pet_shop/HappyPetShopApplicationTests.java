abbb6aeaf1452f00553336ba31f6cc2214e353dc
nhieelam <lam.nhiee11@gmail.com>
2026-08-06 17:03:47 +0700

    Fix test database configuration

--- server/happy-pet-shop/src/test/java/com/funcoders/happy_pet_shop/HappyPetShopApplicationTests.java
+++ server/happy-pet-shop/src/test/java/com/funcoders/happy_pet_shop/HappyPetShopApplicationTests.java
@@ -2,8 +2,10 @@ package com.funcoders.happy_pet_shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HappyPetShopApplicationTests {
 
 	@Test
