4ce17b25d811c8e416d39c2622a0c61abfa6a243
camdao <luongcamdao@gmail.com>
2026-08-07 20:39:24 +0700

    fix: configuration

--- server/happy-pet-shop/src/main/java/com/funcoders/happy_pet_shop/configuration/CorsConfig.java
+++ server/happy-pet-shop/src/main/java/com/funcoders/happy_pet_shop/configuration/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         // Origins must not include a trailing slash
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://new-main.d213ba06r4edsz.amplifyapp.com"
+                "http://pet-shop-frontend.s3-website-ap-southeast-1.amazonaws.com/"
         ));
         configuration.setAllowedMethods(List.of(
                 "GET",
